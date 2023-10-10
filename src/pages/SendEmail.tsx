3384b99b85fd822636c1d7b7ac4a7b4433ad7149
Selim <selimkerimow68@gmail.com>
2023-10-10 20:53:48 +0500

    created email form

--- src/pages/SendEmail.tsx
+++ src/pages/SendEmail.tsx
@@ -1,13 +1,58 @@
 import React from "react";
 
+// css
+import '../css/sendEmail.css'
+
+// Material UI 
+import { TextField } from "@mui/material";
+
+// react-icons
+import { GrSend } from 'react-icons/gr'
+
 
 const SendEmail: React.FC = () => {
 
 
 
     return (
-        <div className="home">
-            SendEmail
+        <div className="email">
+            <h1 className="email--title">You can send me an email directly from this website</h1>
+
+            <form className="email--form">
+
+                <TextField
+                    
+                    required
+                    id="outlined-required"
+                    label="From"
+                    fullWidth={true}
+                />
+                <TextField
+                
+                    disabled
+                    id="outlined-required"
+                    label="To"
+                    defaultValue='selimkerimow68@gmail.com'
+                    fullWidth={true}
+                />
+                <TextField
+                    id="outlined-multiline-flexible"
+                    label="Subject"
+                    multiline
+                    fullWidth={true}
+                />
+                <TextField
+                    required
+                    id="outlined-multiline-static"
+                    label="Compose email"
+                    multiline
+                    rows={7}
+                />
+                <button className="email--send">
+                    Send <GrSend />
+                </button>
+
+            </form>
         </div>
     )
 }
